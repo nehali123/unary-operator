3f9e609ab14e99111ab6269f13b3e5bfc0d19dc3
(neha ali) <159723021+nehali123@users.noreply.github.com>
2024-03-16 21:52:11 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,28 @@
+"use strict";
+//urinary operators
+// increment and decrement operetor // ++ --
+//let count: number= 2;
+// increament the count by 1//
+//count++  //count= count +1 
+//console.log("count after increament:",count);
+// decrement count by 1
+//let remainingItems : number = 8  
+//remainingItems-- 
+//console.log("remaining items after decrement:", remainingItems);
+//post and pre increment -decrement
+//variable ++ (post)  ++variable(pre)
+// --  (post)  --variable(pre)
+let x = 4;
+//post increment
+let postincrement = x++;
+console.log("post increment value", postincrement);
+console.log("value after postincrement", x);
+//pre increment
+let preincrement = ++x;
+console.log("value after preincrement", preincrement);
+let postdecrement = x--;
+console.log("post decrement value", postdecrement);
+console.log("value after postdecrement", x);
+//pre increment
+let predecrement = --x;
+console.log("value after predecrement", predecrement);
---
+++ main.ts
@@ -0,0 +1,43 @@
+//urinary operators
+// increment and decrement operetor // ++ --
+
+//let count: number= 2;
+
+// increament the count by 1//
+
+ //count++  //count= count +1 
+
+ //console.log("count after increament:",count);
+ 
+// decrement count by 1
+//let remainingItems : number = 8  
+
+//remainingItems-- 
+//console.log("remaining items after decrement:", remainingItems);
+
+//post and pre increment -decrement
+//variable ++ (post)  ++variable(pre)
+// --  (post)  --variable(pre)
+
+
+let x : number = 4;
+//post increment
+
+let postincrement: number = x++;
+console.log("post increment value",postincrement);
+console.log("value after postincrement",x);
+
+
+//pre increment
+let preincrement : number = ++x;
+console.log("value after preincrement",preincrement);
+
+
+let postdecrement: number = x--;
+console.log("post decrement value",postdecrement);
+console.log("value after postdecrement",x);
+
+
+//pre increment
+let predecrement : number = --x;
+console.log("value after predecrement",predecrement);
